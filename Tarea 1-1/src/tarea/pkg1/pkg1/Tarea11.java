30a83d1a81c228151995667ec0a446acdc976b7e
Zabdi Javier Andrade Ventura <zandrade@unah.hn>
2024-06-18 21:47:53 -0600

    Subiendo Carpeta de Netbeans

---
+++ Tarea 1-1/src/tarea/pkg1/pkg1/Tarea11.java
@@ -0,0 +1,11 @@
+package tarea.pkg1.pkg1;
+
+public class Tarea11 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Zabdi Ventura"); 
+        
+    }
+    
+}
