93e058dd74968a4a7bbb285709845db98fd3cc6a
Zabdi Javier Andrade Ventura <zandrade@unah.hn>
2024-07-12 23:50:29 -0600

    Tarea 2.2

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,10 @@
+package calculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Calculadora3000 n = new Calculadora3000();
+        n.setVisible(true);
+    }
+    
+}
